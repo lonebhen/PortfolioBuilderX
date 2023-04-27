ab7533b18caebf06ffee2c13ab271587efa47465
lonebhen <ben.angmortey@gmail.com>
2023-04-27 13:13:46 +0000

    ready for deployment

--- builderX/settings.py
+++ builderX/settings.py
@@ -146,13 +146,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
