0d7973c5db1bac365c60636bc88003e2192da6ab
lonebhen <ben.angmortey@gmail.com>
2023-04-27 13:43:37 +0000

    cors

--- builderX/settings.py
+++ builderX/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ah$%=(7asguls4)e)ral(l#c8(_q4m2ymfs^-e+ll-+1hc+8q#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
     'base.apps.BaseConfig',
     'widget_tweaks',
     'rest_framework',
+    'corsheaders',
     
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -171,3 +173,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
